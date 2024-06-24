dd3d0966e461d3175297c629762242fd7917deac
Andrés Zsögön <10298071+andreszs@users.noreply.github.com>
2024-06-24 20:35:29 -0300

    1.0.0

---
+++ com.andreszs.recaptcha.v2.demo/www/js/index.js
@@ -0,0 +1,60 @@
+/* global cordova, device */
+
+var app = {
+	PLUGIN_VERSION: '1.0.0',
+	URL_PLUGIN: 'https://github.com/andreszs/cordova-plugin-recaptcha-v2',
+	URL_DEMO: 'https://www.andreszsogon.com/cordova-plugin-recaptcha-v2-demo/',
+	URL_DEMOS: 'https://github.com/andreszs/cordova-plugin-demos',
+	getVersion: function () {
+		if (typeof (cordova.getAppVersion) != 'undefined') {
+			var onSuccess = function (version) {
+				$('#app_version').html('App version: ' + version);
+			};
+			cordova.getAppVersion.getVersionNumber(onSuccess);
+			$('#plugin_version').html('Plugin version ' + app.PLUGIN_VERSION);
+		} else {
+			$('#versions').hide();
+		}
+	},
+	init: function () {
+		$('#status').html('<span class="success">Device is ready</span>');
+
+		$('#btnVerify').on('click', app.verify);
+
+		$('#btnGitHub').on('click', function () {
+			window.open(app.URL_PLUGIN, '_system');
+		});
+		$('#btnWebsite').on('click', function () {
+			window.open(app.URL_DEMO, '_system');
+		});
+		$('#btnPluginDemos').on('click', function () {
+			window.open(app.URL_DEMOS, '_system');
+		});
+	},
+	verify: function () {
+		var onSuccess = function (strToken) {
+			$('#token').html(strToken);
+			$('#status').html('<span class="success">You have 2 minutes to validate this token as shown in reCAPTCHA &gt; Server Side Validation &gt; Verify the User\'s Response.</span>');
+		};
+
+		var onFailure = function (strError) {
+			$('#status').html('<span class="error">' + strError + '</span>');
+		};
+
+		var args = {};
+		args.siteKeyAndroid = $('#siteKeyAndroid').val();
+		args.siteKeyWeb = $('#siteKeyWeb').val();
+
+		$('#token').empty();
+		$('#status').html('<span class="info">Requesting reCAPTCHA challenge...</span>');
+
+		cordova.plugins.Recaptcha.verify(onSuccess, onFailure, args);
+	}
+};
+
+/* Cordova has been loaded. Perform any initialization that requires Cordova here. */
+document.addEventListener('deviceready', function () {
+	app.init();
+	app.getVersion();
+}, false);
+
